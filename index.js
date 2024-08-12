1b569e12046f9831c80a44cb1b0dc089d54e93c6
Abhisheksahu81 <abhisheksahu812046@gmail.com>
2024-08-12 14:34:26 +0530

    resolved a error in dotenv

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/' , async(req, res) => {
   res.send({"Message" : "running Successfully"});
 })
 
-const PORT = dotenv.parsed.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server runing on port ${PORT}`);
 });
